287c30cd13bdc52a563b06e3f215b138b884a081
Abraham V <abraham.varricatt@googlemail.com>
2018-09-06 21:42:25 -0400

    Autoprefixer + sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
 var path = require("path");
 
 
 const SASS_INCLUDE_PATHS = [
     path.join(__dirname, '/node_modules/')
 ];
+const NODE_SASS_OPTIONS = {
+    includePaths: SASS_INCLUDE_PATHS,
+    outputStyle: "expanded",
+    precision: 6
+};
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function () {
@@ -22,9 +29,14 @@ gulp.task('serve', ['sass'], function () {
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function () {
     return gulp.src("src/scss/*.scss")
-        .pipe(sass({ includePaths: SASS_INCLUDE_PATHS }))
+        .pipe(sourcemaps.init())
+        .pipe(sass(NODE_SASS_OPTIONS).on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write('maps'))
         .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('default', ['serve']);
+
+gulp.task('build', ['sass']);
